86ab2c90ed09ca87b979408b99c09fe50aac4f54
abxnxsh <91929479+abxnxsh@users.noreply.github.com>
2023-06-27 18:04:08 +0530

    Update tempgit.py

--- tempgit.py
+++ tempgit.py
@@ -11,6 +11,7 @@ led=Pin(12,Pin.OUT)
 while True:
     dht_sensor.measure()
     tempr=dht_sensor.temperature()
+    print("Temperature reading: ")
     print(tempr)
     if(tempr>25):
         led.value(1)
